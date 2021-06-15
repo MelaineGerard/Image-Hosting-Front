4d68db13a051fb0c4175d1be7d3a2c5bba38bc47
Melaine Gérard <pro@melaine-gerard.fr>
2021-06-15 10:45:35 +0200

    Added upload page

--- server.js
+++ server.js
@@ -1,39 +1,40 @@
 const config = require('./config.json'),
-  express = require('express'),
-  app = express(),
-  axios = require('axios').default
+    express = require('express'),
+    app = express(),
+    axios = require('axios').default;
+
 
 app.use(express.static('public'))
 app.set('view engine', 'pug')
 
 app.get('/upload', (req, res) => {
-  res.render('upload')
+    res.render('upload')
 })
+
 
 app.get('/:uuid', function (req, res) {
-  let uuid = req.params.uuid
-  let image
-  axios
-    .get(`${config['api']}/images/${uuid}`)
-    .then(function (response) {
-      if (response.data.length !== 0) {
-        console.log(response.data)
-        if (!response.data.message) {
-          res.render('image', response.data)
-          return
-        }
-      }
+    let uuid = req.params.uuid
+    axios
+        .get(`${config['api']}/images/${uuid}`)
+        .then(function (response) {
+            if (response.data.length !== 0) {
+                console.log(response.data)
+                if (!response.data.message) {
+                    res.render('image', response.data)
+                    return
+                }
+            }
 
-      res.redirect(config['redirect_url'])
-    })
-    .catch(function (error) {
-      console.error(error)
-      res.redirect(config['redirect_url'])
-    })
+            res.redirect(config['redirect_url'])
+        })
+        .catch(function (error) {
+            console.error(error)
+            res.redirect(config['redirect_url'])
+        })
 })
 app.route('*').all((req, res) => {
-  res.redirect(config['redirect_url'])
+    res.redirect(config['redirect_url'])
 })
 app.listen(config['port'], () => {
-  console.log(`Server started on port ${config['port']}`)
+    console.log(`Server started on port ${config['port']}`)
 })
