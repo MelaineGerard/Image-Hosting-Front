89b80ec9413a7a02ec867b2f00d7674c688d097c
Melaine Gérard <pro@melaine-gerard.fr>
2021-06-15 02:22:17 +0200

    Update package.json

--- server.js
+++ server.js
@@ -3,31 +3,33 @@ const config = require('./config.json'),
   app = express(),
   axios = require('axios').default
 
-app.use(express.static('public'));
+app.use(express.static('public'))
 app.set('view engine', 'pug')
 
-
-
-app.get("/upload", (req, res) => {
+app.get('/upload', (req, res) => {
   res.render('upload')
 })
 
 app.get('/:uuid', function (req, res) {
   let uuid = req.params.uuid
   let image
-   axios.get(`${config['api']}/images/${uuid}`)
+  axios
+    .get(`${config['api']}/images/${uuid}`)
     .then(function (response) {
-      if(response.data.length !== 0){
+      if (response.data.length !== 0) {
         console.log(response.data)
-        res.render('image', response.data)
-        return
+        if (!response.data.message) {
+          res.render('image', response.data)
+          return
+        }
       }
+
+      res.redirect(config['redirect_url'])
     })
     .catch(function (error) {
       console.error(error)
+      res.redirect(config['redirect_url'])
     })
-    
-    res.redirect(config['redirect_url'])
 })
 
 app.listen(config['port'], () => {
